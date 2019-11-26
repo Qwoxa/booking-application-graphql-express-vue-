85ecebc5d7844112b6a59d39ff1b006acc4b087d
Nick <nick@rhinodispatch.com>
2019-11-26 04:45:48 -0500

    schema

--- server/app.js
+++ server/app.js
@@ -1,4 +1,42 @@
-const express = require('express')
-const bodyParser = require('body-parser')
+const express = require('express');
+const bodyParser = require('body-parser');
+const graphqlHTTP = require('express-graphql');
+const { buildSchema } = require('graphql');
 
-console.log('dsffd')
+const app = express();
+
+app.use(bodyParser.json());
+
+const _store = ['Romantic Cooking', 'Sailing', 'All Night Cooking'];
+
+app.use(
+  '/graphql',
+  graphqlHTTP({
+    schema: buildSchema(`
+    type RootQuery {
+      events: [String!]!
+    }
+
+    type RootMutation {
+      createEvent(name: String!): [String]
+    }
+
+    schema {
+      query: RootQuery
+      mutation: RootMutation
+    }
+  `),
+    rootValue: {
+      events: () => {
+        return _store;
+      },
+      createEvent: ({ name }) => {
+        _store.push(name);
+        return _store;
+      },
+    },
+    graphiql: true,
+  })
+);
+
+app.listen(3000);
